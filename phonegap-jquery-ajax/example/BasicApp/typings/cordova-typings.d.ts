b8087fb2b9cada83a6542b3df8bd86d8f9de5470
Christian Marienfeld <post@chrisland.de>
2017-10-28 11:32:08 +0200

    jquery.ajax app

---
+++ phonegap-jquery-ajax/example/BasicApp/typings/cordova-typings.d.ts
@@ -0,0 +1,16 @@
+
+/// <reference path="../.vscode/typings/cordova/cordova.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/plugins/FileSystem.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/plugins/DeviceMotion.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/plugins/Contacts.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/plugins/Device.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/plugins/DeviceOrientation.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/plugins/Dialogs.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/plugins/FileTransfer.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/plugins/Globalization.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/plugins/InAppBrowser.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/plugins/Media.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/plugins/NetworkInformation.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/plugins/Splashscreen.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/plugins/StatusBar.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/plugins/Vibration.d.ts"/>
